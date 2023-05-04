118b795ff79d7a7cf829cc33aedc1926cc9bd867
DiepNguyenBillidentity <diep.nguyen@billidentity.com>
2023-05-04 09:38:45 +0700

    Update Camera

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://127.0.0.1:8102',
+  },
 };
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
+// Call the element loader after the platform has been bootstrapped
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+defineCustomElements(window);
